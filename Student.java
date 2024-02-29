478bc2008024bf0a3ebce10ae11ef24178511590
Tran Trong Nghia <nghiaasi4n@gmail.com>
2024-02-29 14:19:02 +0700

    add file student.java

---
+++ Student.java
@@ -0,0 +1,21 @@
+public class Student {
+    //props
+    private String id;
+    private String name;
+    private String add;
+
+    //methods
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("stop");
+    }
+}
+
+
